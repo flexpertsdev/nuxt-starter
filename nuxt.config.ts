a23471f25104e3d17e1d32c76268182b2ccedb3c
flexpertsdev <155709656+flexpertsdev@users.noreply.github.com>
2025-07-03 02:59:25 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,16 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+   content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
 })
+
+
